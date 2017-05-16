88f793af62b15c937704ca8cf811609d51db2f37
周艺伟 <ywzhou@yilepay.com>
2017-05-16 09:49:05 +0800

    动态修改日志

---
+++ dynamic_log/src/main/java/com/spring/cloud/book/test/DynamicLogApplication.java
@@ -0,0 +1,18 @@
+package com.spring.cloud.book.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DynamicLogApplication
+ * @author zhouyw
+ * @date 2017.05.16
+ */
+@SpringBootApplication
+public class DynamicLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicLogApplication.class, args);
+    }
+
+}
---
+++ dynamic_log/src/main/java/com/spring/cloud/book/test/web/LogController.java
@@ -0,0 +1,32 @@
+package com.spring.cloud.book.test.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * LogController
+ * @author zhouyw
+ * @date 2017.05.16
+ */
+@RestController
+@RequestMapping(value = "/log")
+public class LogController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 默认的日志级别为INFO
+     * @return
+     */
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String testLogLevel() {
+        logger.debug("Logger Level ：DEBUG");
+        logger.info("Logger Level ：INFO");
+        logger.error("Logger Level ：ERROR");
+        return "welcome to log controller";
+    }
+
+}
